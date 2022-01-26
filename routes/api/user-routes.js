c48559e69ff495ca6dcdacd547a6ec0742f35087
Wenwen Tian <wwtian9@gmail.com>
2022-01-26 16:02:12 -0700

    add user model

---
+++ routes/api/user-routes.js
@@ -0,0 +1,104 @@
+const router = require("express").Router();
+const { User } = require("../../models");
+
+// This naming convention along with the use of the HTTP methods follow a famous API architectural pattern called REST, or Representational State Transfer.
+// APIs built following this pattern are what's known as RESTful APIs.
+// HTTP methods: GET POST PUT DELETE
+
+// GET /api/users
+router.get("/", (req, res) => {
+  // Access our User model and run .findAll() method
+  // .findAll()方法允许我们从数据库中的用户表中查询所有用户,等于 SELECT * FROM users;
+  User.findAll()
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// GET /api/users/1
+router.get("/:id", (req, res) => {
+  User.findOne({
+    // where option to indicate we want to find a user where its id value equals whatever req.params.id is,
+    // like the SQL query: SELECT * FROM users WHERE id = 1
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// POST /api/users
+router.post("/", (req, res) => {
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// PUT /api/users/1
+router.put("/:id", (req, res) => {
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  // UPDATE users
+  // SET username = "Lernantino", email = "lernantino@gmail.com", password = "newPassword1234"
+  // WHERE id = 1;
+
+  // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+  User.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData[0]) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// DELETE /api/users/1
+// .destroy()方法并提供某种类型的标识符来指示我们希望从用户数据库表中删除数据的确切位置。
+router.delete("/:id", (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
