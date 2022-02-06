693d74c1b9ac3f49a3756dc34be808ba87f39c0c
Wenwen Tian <wwtian9@gmail.com>
2022-02-06 11:32:37 -0700

    add single post

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,35 +1,53 @@
-async function signupFormHandler(event) {
+async function loginFormHandler(event) {
   event.preventDefault();
 
-  // grab user data from the form
-  const username = document.querySelector("#username-signup").value.trim();
-  const email = document.querySelector("#email-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
 
-  //added a conditional to make sure that all fields have values before making the POST request.
-  if (username && email && password) {
-    // add the await keyword before the Promise
-    const response = await fetch("/api/users", {
-      method: "post",
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'post',
       body: JSON.stringify({
-        username,
         email,
-        password,
+        password
       }),
-      headers: { "Content-Type": "application/json" },
+      headers: { 'Content-Type': 'application/json' }
     });
-    // check the response status
+
     if (response.ok) {
-      // successful login returns to home page
-      document.location.replace("/");
+      document.location.replace('/');
     } else {
       alert(response.statusText);
     }
   }
 }
 
-document
-  .querySelector(".signup-form")
-  .addEventListener("submit", signupFormHandler);
+async function signupFormHandler(event) {
+  event.preventDefault();
 
-  
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (username && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
