c94ac7936ea60049a49579ce65de47ef13350dcf
Wenwen Tian <wwtian9@gmail.com>
2022-02-09 15:57:47 -0700

    create post edit page

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -4,25 +4,22 @@ async function newFormHandler(event) {
   const title = document.querySelector('input[name="post-title"]').value;
   const post_url = document.querySelector('input[name="post-url"]').value;
 
-  //从表单中获取post-title和post-url值并将它们与 POST 请求一起发送到/api/posts
   const response = await fetch(`/api/posts`, {
-    method: "POST",
+    method: 'POST',
     body: JSON.stringify({
       title,
-      post_url,
+      post_url
     }),
     headers: {
-      "Content-Type": "application/json",
-    },
+      'Content-Type': 'application/json'
+    }
   });
 
   if (response.ok) {
-    document.location.replace("/dashboard");
+    document.location.replace('/dashboard');
   } else {
     alert(response.statusText);
   }
 }
 
-document
-  .querySelector(".new-post-form")
-  .addEventListener("submit", newFormHandler);
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
